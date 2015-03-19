525c064fbd8d1d8de84dc6837ad8d253a8b54b66
adarmon <axeldarmon@aol.com>
2015-03-19 09:40:17 +0100

    add autoload

--- includes/functions.php
+++ includes/functions.php
@@ -1,8 +1,22 @@
 ﻿<?php
 // constantes
 define('ROOTDIR', $_SERVER['DOCUMENT_ROOT']);
+define('CLASSES_DIR', ROOTDIR.'/models');
 
-// debug
+/** autoload
+ * 
+ * @param type $classname
+ */
+function __autoload($classname) {
+    require(CLASSES_DIR . '/' . $classname . '.php');
+}
+
+/**
+ * debug
+ * 
+ * @param type $tab
+ * @return boolean
+ */
 function ff($tab) {
     if ($tab) {
         $e = "<pre>";
@@ -14,7 +28,13 @@ function ff($tab) {
     return false;
 }
 
-// add message
+/**
+ * add message
+ * 
+ * @param type $code
+ * @param type $type
+ * @param type $message
+ */
 function addMessage($code, $type, $message) {
     $_SESSION['msg'][] = array(
         'code' => $code,
@@ -22,7 +42,11 @@ function addMessage($code, $type, $message) {
         'label' => $message);
 }
 
-// return area of a circle
+/** return area of a circle
+ * 
+ * @param type $r
+ * @return type
+ */
 function circleArea($r) {
     $area = M_PI * pow($r, 2);
     return $area;
