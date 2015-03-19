72e20a07555b3be3fe34018212673edab9215b36
adarmon <axeldarmon@aol.com>
2015-03-19 09:11:02 +0100

    creation of class Article

---
+++ classes/Article.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Article
+ *
+ * @author Ariel
+ */
+class Article {
+    
+    // attributes
+    public $id;
+    public $title;
+    public $content;
+    
+    // methods
+    /**
+     * 
+     * @param type $id
+     */
+    public function displayArticle($id) {
+        
+    }
+    
+    /**
+     * 
+     */
+    public function addArticle() {
+        
+    }
+    
+    /**
+     * 
+     * @param type $id
+     */
+    public function deleteArticle($id) {
+        
+    }
+    
+    /**
+     * 
+     * @param type $id
+     */
+    public function editArticle($id) {
+        
+    }
+}
